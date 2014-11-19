f1076e8cdc2f49103d6593528fe3381d3971dca1
LuisOsvaldo <luis.serna@truextend.com>
2014-11-19 18:56:08 -0400

    the test as a parametrized test (annotate with an @RunWith(Parameterized.class) and add a method to provide 10 empty parameters). That way the framework will run the test 10 times

--- src/test/java/com/eviltester/webdriver/MyFirstTest.java
+++ src/test/java/com/eviltester/webdriver/MyFirstTest.java
@@ -2,13 +2,28 @@ package com.eviltester.webdriver;
 
 import junit.framework.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+
+
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Created by luis.serna on 11/6/2014.
  */
+@RunWith(Parameterized.class)
 public class MyFirstTest {
+
+    @Parameterized.Parameters
+    public static List<Object[]> data() {
+        return Arrays.asList(new Object[10][0]);
+    }
+
+    public MyFirstTest() {
+    }
 
     @Test
     public void startWebDriver(){
